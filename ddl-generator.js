1b1e50dac54a77f5386c38d7f426169f5fdd417d
Your Name~ <you@example.com>
2022-02-20 08:12:14 -0300

    ss

--- ddl-generator.js
+++ ddl-generator.js
@@ -50,20 +50,19 @@ class DDLGenerator {
         console.log('templates', results);
 
         // Templates
-      results.forEach(t ,basePath, elem   => {
+        for (var i = 0; i < results.length; i++) {
+            var t = results[i]
+            console.log(t)
+            var template = Twig.twig({
+              data:  fs.readFileSync( t  ).toString()
+            });
 
-          console.log(t)
-          var template = Twig.twig({
-            data:  fs.readFileSync( t  ).toString()
-          });
+            var myPath = path.join( this.basePath , elem.name ,  relativePath(t,'/templates/').split('.')[0] ) 
+            console.log (myPath)
 
-          var myPath = path.join( this.basePath , elem.name ,  relativePath(t,'/templates/').split('.')[0] ) 
-          console.log (myPath)
+            fs.writeFileSync( myPath,  template.render(elem), { recursive: true })
 
-          fs.writeFileSync( myPath,  template.render(elem), { recursive: true })
-
-      },  this.basePath , elem )
-
+       }
 
 
       });
